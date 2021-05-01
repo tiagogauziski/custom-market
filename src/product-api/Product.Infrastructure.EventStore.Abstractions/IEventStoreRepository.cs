5beb28ed56283e88ac61666a755603b188b300c7
Tiago Gauziski <tiago.gauziski@gmail.com>
2021-05-01 21:29:16 +1200

    implementing event store data store in mongodb, work in progress.

---
+++ src/product-api/Product.Infrastructure.EventStore.Abstractions/IEventStoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Product.Application.Event.Common;
+
+namespace Product.Infrastructure.EventStore.Abstractions
+{
+    /// <summary>
+    /// Abstraction of repository to save events into data store.
+    /// </summary>
+    public interface IEventStoreRepository
+    {
+        /// <summary>
+        /// Save an event into data store.
+        /// </summary>
+        /// <param name="systemEvent">Event being stored.</param>
+        /// <param name="cancellation">Cancellation token.</param>
+        /// <returns>A task representing an asynchronous operation.</returns>
+        Task SaveEvent(EventBase systemEvent, CancellationToken cancellation);
+    }
+}

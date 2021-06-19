a4a6f7d6b27866a79c3f0778bf94a79f52d800ac
Tiago Gauziski <tiago.gauziski@gmail.com>
2021-06-19 23:24:10 +1200

    tests for product service.

---
+++ src/Stock.API/Stock.Application.UnitTests/Product/Service/ProductServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using Stock.Application.Product.Service;
+using Xunit;
+
+namespace Stock.Application.UnitTests.Product.Service
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public void Ctor_WhenNullProductServiceSettings_ShouldReturnException()
+        {
+            // Arrange
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                ProductService productService = new ProductService(Mock.Of<HttpClient>(), null);
+            });
+        }
+    }
+}

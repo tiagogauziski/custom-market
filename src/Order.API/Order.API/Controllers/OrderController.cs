1c959c1a5e636a90b84ed80dea90727aa6567df0
Tiago Gauziski <tiago.gauziski@gmail.com>
2021-06-24 22:58:38 +1200

    add application layer. add order controller and start designing the commands.

---
+++ src/Order.API/Order.API/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Order.Application.Order.Commands;
+
+namespace Order.API.Controllers
+{
+    /// <summary>
+    /// API endpoints for handling with orders.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// Endpoint to create an order.
+        /// </summary>
+        /// <param name="createOrderCommand">Create order command.</param>
+        /// <returns>HTTP response.</returns>
+        [HttpPost]
+        public Task<IActionResult> Create(CreateOrderCommand createOrderCommand)
+        {
+            return Task.FromResult(Ok() as IActionResult);
+        }
+    }
+}
---
+++ src/Order.API/Order.Application/Order/Commands/CreateOrderCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Order.Application.Order.Commands.Contracts;
+
+namespace Order.Application.Order.Commands
+{
+    public class CreateOrderCommand
+    {
+        /// <summary>
+        /// Gets or sets the customer details.
+        /// </summary>
+        public CustomerContract Customer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delivery address.
+        /// </summary>
+        public AddressContract DeliveryAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the billing address.
+        /// </summary>
+        public AddressContract BillingAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the payment details.
+        /// </summary>
+        public PaymentDetailContract PaymentDetail { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of items in the order.
+        /// </summary>
+        public IEnumerable<OrderItemContract> OrderItems { get; set; }
+
+    }
+}
